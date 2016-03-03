0dcc4847e640fc480a625c1baca04332d6285185
Nathan Denig <ngduser@gmail.com>
2016-03-03 14:16:46 -0600

    Added Missing Header Comments

--- Jobs.java
+++ Jobs.java
@@ -1,6 +1,9 @@
+// File: Jobs.java
+// Date: Feb 29, 2016
+// Author: Nathan Denig
+// Purpose: Create Job Threads for creature setJobs method
 
-
-public class Jobs extends Thread {
+public class Jobs extends Thread{
     
     int index, creature_index;
     String name;
